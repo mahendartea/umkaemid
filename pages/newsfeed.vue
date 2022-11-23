ffbfb96483c9ac176ed2da6839cb48f182979bf9
Mahendar Dwi Payana <hendartea@gmail.com>
2022-11-23 16:25:06 +0700

    inisiasi newsfeed

--- pages/newsfeed.vue
+++ pages/newsfeed.vue
@@ -1,5 +1,75 @@
 <template>
   <NuxtLayout>
-    <h1>Newsfeed</h1>
+    <div class="flex flex-wrap justify-center">
+      <div
+        class="w-full lg:w-5/6 p-2 flex flex-col justify-center items-center"
+      >
+        <p
+          class="text-xl p-2 bg-yellow-500 text-white rounded-sm my-4 w-full text-center shadow-lg mx-2"
+        >
+          Newsfeed
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-wrap justify-center mb-5 p-2 space-y-4">
+      <div
+        class="w-full lg:w-5/6 px-10 py-5 flex items-center justify-center border border-slate-300 shadow-lg rounded-lg"
+      >
+        <div class="w-1/5 rounded-lg overflow-auto">
+          <img
+            src="https://uemkaem.id/images/logo/thumbnail/1661157242.png"
+            alt="profil"
+            width="200"
+          />
+        </div>
+        <div
+          class="w-5/6 flex flex-col items-start justify-start content-start px-5 h-full"
+        >
+          <h1 class="text-2xl fond-bold mb-8">
+            Berdayakan Ekonomi Usaha Teman Sekolah, Ikata...
+          </h1>
+          <hr />
+          <p class="text-sm my-3">
+            Ikatan Alumni (ILUNI) terbentuk umumnya bertujuan menjalin tali
+            silaturahmi agar tetap kompak dan solid di angkatan tiap sekolah
+            atau organisasi.
+          </p>
+          <div class="w-full flex items-center justify-between">
+            <p class="text-md">---Contributor..</p>
+            <p class="text-xs text-slate-400">29 Mar 2022 - 09:59</p>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="w-full lg:w-5/6 px-10 py-5 flex items-center justify-center border border-slate-300 shadow-lg rounded-lg"
+      >
+        <div class="w-1/5 rounded-lg overflow-auto">
+          <img
+            src="https://uemkaem.id/images/logo/thumbnail/1661157242.png"
+            alt="profil"
+            width="200"
+          />
+        </div>
+        <div
+          class="w-5/6 flex flex-col items-start justify-start content-start px-5 h-full"
+        >
+          <h1 class="text-2xl fond-bold mb-8">
+            Berdayakan Ekonomi Usaha Teman Sekolah, Ikata...
+          </h1>
+          <hr />
+          <p class="text-sm my-3">
+            Ikatan Alumni (ILUNI) terbentuk umumnya bertujuan menjalin tali
+            silaturahmi agar tetap kompak dan solid di angkatan tiap sekolah
+            atau organisasi.
+          </p>
+          <div class="w-full flex items-center justify-between">
+            <p class="text-md">---Contributor..</p>
+            <p class="text-xs text-slate-400">29 Mar 2022 - 09:59</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </NuxtLayout>
+  <DivFooter />
 </template>
